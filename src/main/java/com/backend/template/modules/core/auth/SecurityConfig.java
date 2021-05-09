48f9c13d3dbd48664cfa651677f6d9d7cf66deb0
duc anh <chuducanh@gmail.com>
2021-05-09 22:54:11 +0700

    cors

--- src/main/java/com/backend/template/modules/core/auth/SecurityConfig.java
+++ src/main/java/com/backend/template/modules/core/auth/SecurityConfig.java
@@ -21,6 +21,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -78,6 +83,18 @@ public class SecurityConfig {
             web.ignoring().antMatchers(AUTH_WHITELIST);
         }
 
+        @Bean
+        CorsConfigurationSource corsConfigurationSource() {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOrigins(Arrays.asList("*"));
+            configuration.setAllowedMethods(Arrays.asList("*"));
+            configuration.setAllowedHeaders(Arrays.asList("*"));
+            configuration.setAllowCredentials(true);
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", configuration);
+            return source;
+        }
+
         @Override
         protected void configure(HttpSecurity http) throws Exception {
 
@@ -87,7 +104,7 @@ public class SecurityConfig {
                     .and()
                     .exceptionHandling()
                     .authenticationEntryPoint(customRestExceptionHandler)
-                    .and().csrf().disable().cors().disable()
+                    .and().csrf().disable().cors().and()
                     .authorizeRequests()//.antMatchers(AUTH_WHITELIST).permitAll()
                     .antMatchers("/auth/login").permitAll()
                     .antMatchers("/user/register").permitAll()
