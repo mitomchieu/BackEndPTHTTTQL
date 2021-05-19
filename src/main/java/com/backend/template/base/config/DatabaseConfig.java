ff4444480a8e2b8107f6a1657b4f4fd340bbfc9a
duc anh <chuducanh@gmail.com>
2021-05-19 07:57:36 +0700

    fix convention

--- src/main/java/com/backend/template/base/config/DatabaseConfig.java
+++ src/main/java/com/backend/template/base/config/DatabaseConfig.java
@@ -15,13 +15,9 @@ public class DatabaseConfig {
     public DataSource getDataSource() {
         DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-//        dataSourceBuilder.url(EnvConst.DATABASE_URL);
-//        dataSourceBuilder.username(EnvConst.DATABASE_USERNAME);
-//        dataSourceBuilder.password(EnvConst.DATABASE_PASSWORD);
-
-        dataSourceBuilder.url("jdbc:mysql://ec2-13-212-58-46.ap-southeast-1.compute.amazonaws.com:3306/sample");
-        dataSourceBuilder.username("root");
-        dataSourceBuilder.password("12!@qwER");
+        dataSourceBuilder.url(EnvConst.DATABASE_URL);
+        dataSourceBuilder.username(EnvConst.DATABASE_USERNAME);
+        dataSourceBuilder.password(EnvConst.DATABASE_PASSWORD);
         return dataSourceBuilder.build();
     }
 
